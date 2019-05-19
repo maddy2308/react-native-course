39100dee355431e03b92ba13a3d42e6e90c2e402
maddy2308 <madhur.eng@gmail.com>
2019-05-19 14:50:58 -0400

    React Native's first component - Header Component

---
+++ index.js
@@ -0,0 +1,20 @@
+// Import library to help create a component
+import React from 'react'
+import {View, AppRegistry} from 'react-native'
+import Header from './src/components/Header'
+import AlbumList from './src/components/AlbumList'
+
+
+// Create a component
+const App = () => {
+
+	return (
+		<View>
+			<Header headerText={'Albums'}/>
+		</View>
+	)
+};
+
+
+// Render component to the device
+AppRegistry.registerComponent('albums', () => App);
